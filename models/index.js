68ae8add0e85c07c2774b6098223b78e710264d2
Logan McDaniel <lsmcd@pm.me>
2024-04-01 18:52:49 -0600

    add database columns/associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,20 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(ProductTag, {
+  through: "tag_id"
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(ProductTag, {
+  through: "product_id"
+});
 
 module.exports = {
   Product,
